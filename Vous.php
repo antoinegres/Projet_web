5b3baecf6db939f2db11e558bd14383f0ff8922d
Leroy <lramkalia@gmail.com>
2018-05-03 16:08:40 +0200

    aere

--- Vous.php
+++ Vous.php
@@ -1,46 +1,16 @@
 <!DOCTYPE html>
-<?php
-
-$bdd = new PDO('mysql:host=127.0.0.1;dbname=bddece;charset=utf8', 'root', '');
-if(isset($_POST['submit_recherche'])){
-	header("Location:dpconnect.php");
-}
-?>
-
-
 <html>
 	<head>
 		<meta charset="utf-8" />
-		<title>Formulaire</title>
+		<title>Vous</title>
 		<link rel="stylesheet" type="text/css" href="Accueil.css"/>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	</head>
 	
 	<body>
-	<p>...</p>
-		<div id="corps" align="center">
-			<form method="POST" action=""><br/>
-				<label>Nouvelle Recherche :</label>
-				<input type="submit" value="Valider" name="submit_recherche" id="submit_recherche"/>
-			</form>
-			<?php 
-			if (isset($erreur1))
-			{
-				echo '<font color="red">'.$erreur1."</font>";
-			}
-			if (isset($erreur1))
-			{
-				echo '<font color="red">'.$erreur1."</font>";
-			}
-			if (isset($erreur2))
-			{
-				echo '<font color="red">'.$erreur2."</font>";
-			}
-			if (isset($ok))
-			{
-				echo '<font color="green">'.$ok."</font>";
-			}
-			?>
+		<p>hgh</p>
+		<div id="corps" align="center"><br/><br/>
+			<h1>Mon Profil</h1><br/><br/>
 			<?php
 			
 			try
@@ -57,21 +27,59 @@ if(isset($_POST['submit_recherche'])){
 			// Si tout va bien, on peut continuer
 			// On récupère tout le contenu de la table jeux_video
 			session_start();
-			$blaze=$_SESSION['nom'];
-			$reponse1 = $bdd->query("SELECT * FROM utilisateur WHERE nom='$blaze'");
+			$blaze1=$_SESSION['mail'];
+			$reponse1 = $bdd->query("SELECT * FROM utilisateur WHERE mail='$blaze1'");
 
 			// On affiche chaque entrée une à une
 			while ($donnees = $reponse1->fetch())
 			{
 			?>
 				<p>
-				<strong>Ami</strong> : <a href=Connection.php><?php echo $donnees['nom']; ?>   <?php echo $donnees['prenom']; ?></a>   <?php echo $donnees['statut']; ?>
+				<table>
+					<tr>
+						<td>
+							<label for="nom">Nom :</label>
+						</td>
+						<td>
+							<?php echo $donnees['nom']; ?>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<label for="prenom">Prénom :</label>
+						</td>
+						<td>
+							<?php echo $donnees['prenom']; ?>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<label for="age">Age :</label>
+						</td>
+						<td>
+							<?php echo $donnees['age']; ?>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<label for="mail">E-mail :</label>
+						</td>
+						<td>
+							<?php echo $donnees['mail']; ?>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<label for="statut">Statut :</label>
+						</td>
+						<td>
+							<?php echo $donnees['statut']; ?>
+						</td>
+					</tr>
+				</table>
 			   </p>
-			<?php
-			}
-
-			
-			?>
+			  <?php
+			}?>
 		</div>
 		<div id="menu">
 			<div style="border-color: black" id="menu0"><a href="Accueil.html" >Accueil</a><div>
--- dpconnect.php
+++ dpconnect.php
@@ -93,9 +93,9 @@ if(isset($_POST['submit_recherche'])){
 			?>
 		</div>
 		<div id="menu">
-			<div style="border-color: black" id="menu0"><a href="Accueil.html.html" >Accueil</a><div>
-			<div style="border-color: black" id="menu1"><a href="Mon reseau.html.html">Mon Réseau</a><div>
-			<div style="border-color: black" id="menu2"><a href="Vous.html" >Vous</a><div>
+			<div style="border-color: black" id="menu0"><a href="Accueil.html" >Accueil</a><div>
+			<div style="border-color: black" id="menu1"><a href="dpconnect.php">Mon Réseau</a><div>
+			<div style="border-color: black" id="menu2"><a href="Vous.php" >Vous</a><div>
 			<div style="border-color: black" id="menu3"><a href="Notifications.html" >Notifications</a><div>
 			<div style="border-color: black" id="menu4"><a href="Messages.html" >Messages</a><div>
 			<div style="border-color: black" id="menu5"><a href="Emplois.html" >Emplois</a><div>
